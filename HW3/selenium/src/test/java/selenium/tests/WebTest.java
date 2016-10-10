0bf9384908c1e82e7c4dee384503bee404c79990
mnsekh111 <mnsekh111@gmail.com>
2016-10-09 22:50:18 -0400

    Selenium 2 and 3

--- HW3/selenium/src/test/java/selenium/tests/WebTest.java
+++ HW3/selenium/src/test/java/selenium/tests/WebTest.java
@@ -58,19 +58,33 @@ public class WebTest {
     public void participantCount() throws Exception {
         driver.get("http://www.checkbox.io/studies.html");
         WebDriverWait wait = new WebDriverWait(driver, 30);
-        String surveyName = "Frustration of Software Developers";
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()[contains(.,'Frustration of Software Developers')]]/ancestor::div[contains(@class,'row')]/div[@class='span4']/p/span")));
         List<WebElement> values = driver.findElements(By.xpath("//*[text()[contains(.,'Frustration of Software Developers')]]/ancestor::div[contains(@class,'row')]/div[@class='span4']/p/span"));
 
         //System.out.println(values.get(0).getText());
         assertNotNull(values);
-        assertEquals(values.get(0).getText().trim().compareToIgnoreCase("55"),0);
+        assertEquals(values.get(0).getText().trim().compareToIgnoreCase("55"), 0);
         //$x("//*[text()[contains(.,'Program Comprehension Exercise')]]/ancestor::div[contains(@class,'row')]/div[@class='span4']/p/span/text()")[0]
         //$x("//*[@class='row']/div[position()=2]/a/span/text()[contains(.,'OPEN')]")
         //$x("//*[@class='row']/div[position()=2]/a/span/text()[contains(.,'OPEN')]/../../..//button").length
         //$x("//*[@class='row']/div[position()=2]/a/span/text()[contains(.,'OPEN')]/ancestor::div[@class='span4']//button")
         //$x("//*[text()[contains(.,'Software Changes Survey')]]/ancestor::div[contains(@class,'row')]//span/text()[contains(.,'Amazon')]")
         //assertNotNull(element);
+    }
+
+    @Test
+    public void openClickable() throws Exception {
+        driver.get("http://www.checkbox.io/studies.html");
+
+        // http://geekswithblogs.net/Aligned/archive/2014/10/16/selenium-and-timing-issues.aspx
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='status']/span[.='OPEN']")));
+        List<WebElement> spans = driver.findElements(By.xpath("//a[@class='status']/span[.='OPEN']"));
+        List<WebElement> buttons = driver.findElements(By.xpath("//a[@class='status']/span[.='OPEN']/ancestor::div[@class='span4']//button"));
+
+        assertNotNull(spans);
+        assertNotNull(buttons);
+        assertEquals(buttons.size(), spans.size());
     }
 
 }
